c45ff7b0a73f404032a6847579cba4df07cd56b7
Rony0292 <rony0292@yahoo.com>
2014-02-27 11:16:05 +0100

    run in t server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,44 @@ namespace Http
     {
         static void Main(string[] args)
         {
+
+            string name = "localhost";
+            IPAddress[] addrs = Dns.GetHostEntry(name).AddressList;
+
+            Console.WriteLine(addrs[0]);
+            Console.WriteLine(addrs[1]);
+
+            //TcpListener serverSocket = new TcpListener(addrs[1], 6789);
+
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+            while (true)
+            {
+                Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated");
+
+                Stream ns = new NetworkStream(connectionSocket);
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true; // enable automatic flushing
+
+                string message = sr.ReadLine();
+                string answer = "";
+                while (message != null && message != "")
+                {
+                    Console.WriteLine("Client: " + message);
+                    answer = message.ToUpper();
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+
+                }
+                connectionSocket.Close();
+                ns.Close();
+            }
+
+            serverSocket.Stop();
+
         }
     }
 }
--- WebServerhelper.cs
+++ WebServerhelper.cs
@@ -13,9 +13,7 @@ namespace Http
 {
     public class WebServerhelper
     {
-         //public static  int DEFAULT_PORT = 8888;
-    //private static String ROOT_CATALOG = "C:/";
-    //private static  String CRLF = "\r\n";
+      
         private Socket connectionSocket;
 
         public WebServerhelper(Socket connectinSocket)
@@ -32,7 +30,9 @@ namespace Http
             Console.WriteLine(addrs[0]);
             Console.WriteLine(addrs[1]);
 
-            //TcpListener serverSocket = new TcpListener(addrs[1], 6789);
+            //TcpListener welcomeSocket = new TcpListener(addrs[1], 6789);
+            //IPAddress ip = IPAddress.Parse("127.0.0.1");
+            //TcpListener serverSocket = new TcpListener(ip, 6789);
 
             TcpListener serverSocket = new TcpListener(6789);
             serverSocket.Start();
@@ -40,29 +40,34 @@ namespace Http
             while (true)
             {
                 Socket connectionSocket = serverSocket.AcceptSocket();
-                Console.WriteLine("Server activated");
+                Console.WriteLine("Server activated now");
+                WebServerhelper service = new WebServerhelper(connectionSocket);
+                service.DoIt();
 
-                Stream ns = new NetworkStream(connectionSocket);
-                StreamReader sr = new StreamReader(ns);
-                StreamWriter sw = new StreamWriter(ns);
-                sw.AutoFlush = true; // enable automatic flushing
-
-                string message = sr.ReadLine();
-                string answer = "";
-                while (message != null && message != "")
-                {
-                    Console.WriteLine("Client: " + message);
-                    answer = message.ToUpper();
-                    sw.WriteLine(answer);
-                    message = sr.ReadLine();
-
-                }
-                connectionSocket.Close();
-                ns.Close();
             }
 
             serverSocket.Stop();
 
+        }
+
+        private void DoIt()
+        { 
+            Stream ns = new NetworkStream(connectionSocket);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer;
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+            connectionSocket.Close();
         }
     }
 }
